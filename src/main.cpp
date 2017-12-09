a90feffd644ce526243bee12a7d0fb6df6646cc9
Elorl <elor14@gmail.com>
2017-12-10 01:11:03 +0200

    handle with client part

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 #include "AIPlayer.h"
 #include "ManualPlayer.h"
 #include "ConsoleUI.h"
-#include "Client.h"
-#include <utility>
+#include "RemotePlayer.h"
 #include <libio.h>
 #include <cstdio>
 #include <cstdlib>
+#include <fstream>
 
 
 /*****************************************************************************************************
@@ -19,7 +19,7 @@
 * the output: the function will print the board with the lines and rows numbers, and the boards      *
 * the function operation: the function will create a board object and will run the printBoard func   *
 *****************************************************************************************************/
-pair<const char*, int> readFIle();
+pair<string, int> readFIle();
 
 int main() {
     //ManualPlayer mP1(Black);
@@ -27,68 +27,63 @@ int main() {
     Board b(DEFAULT_SIZE,DEFAULT_SIZE);
     Board *board = &b;
     ConsoleUI consoleUI;
-    ManualPlayer manualPlayer1(Black, consoleUI);
-    Player *mp1 = &manualPlayer1;
-    Player* p2;
-    AIPlayer* ai;
-    ManualPlayer* p3;
+    Player *p1, *p2;
+    Client *client;
     int i = consoleUI.choosePlayerType();
     switch (i) {
         case 1:
-            p3 = new ManualPlayer(White, consoleUI);
-            p2 = p3;
+            p1 = new ManualPlayer(Black, consoleUI);
+            p2 = new ManualPlayer(White, consoleUI);
+            break;
         case 2:
-            ai =  new AIPlayer(White, board);
-            p2 = ai;
+            p1 = new ManualPlayer(Black, consoleUI);
+            p2 =  new AIPlayer(White, board);
+            break;
         case 3:
-            pair <const char*, int> pair;
+            pair <string, int> pair;
             pair = readFIle();
-            Client client(pair.first, pair.second);
+            client = new Client(pair.first.c_str(), pair.second);
+            int number = client->connectToServer();
+            if(number == 1) {
+                p1 = new ManualPlayer(Black, consoleUI, client);
+                p2 = new RemotePlayer(White, consoleUI, client);
+            } else if(number == 2) {
+                p1 = new RemotePlayer(Black, consoleUI, client);
+                p2 = new ManualPlayer(White, consoleUI, client);
+            }
+            break;
     }
-    FlipMoveLogic logic(board, mp1, p2);
+    FlipMoveLogic logic(board, p1, p2);
     GameLogic *l;
     l = &logic;
     p2->setLogic(l);
-    Game game(board, mp1, p2, l, consoleUI);
+    Game game(board, p1, p2, l, consoleUI);
     game.run();
-    if(i == 2) {
-        delete ai;
-    } else {
-        delete p3;
+    //check if there is a client object inside the players.
+    if(p1->sendToServer() || p2->sendToServer()) {
+        delete(client);
     }
+    delete(p1);
+    delete(p2);
 }
 
-pair<const char*, int> readFIle() {
-    int i = 0, prt;
+pair<string, int> readFIle() {
+    int i = 0;
     bool flag = false;
-    pair <const char*, int> pair;
-    FILE *settings;
-    settings = fopen("../settings.txt", "r");
-    char ip[15], port[5];
-    char buffer[1];
-    //check if the opening of the source file have been failed.
-    if (settings == NULL) {
-        printf("there is no settings file.");
-        pair.first = NULL;
-        pair.second = NULL;
+    pair <string, int> pair;
+    ifstream settings;
+    string ip, port;
+    settings.open("../settings.txt");
+    if(settings == NULL) {
+        cout << "there is no settings file.";
+        pair.first = "-1";
+        pair.second = -1;
     } else {
-        while (fread(buffer, 1, 1, settings)) {
-            if(buffer[0] == ',') {
-                ip[i] = '\0';
-                flag = true;
-                i = 0;
-            } else {
-                if(!flag) {
-                    ip[i] = buffer[0];
-                    i++;
-                } else {
-                    port[i] = buffer[0];
-                    i++;
-                }
-            }
-        }
-        pair.first = ip;
-        pair.second = atoi(port);
+        getline(settings,ip);
+        getline(settings,port);
     }
+    int prt = atoi(port.c_str());
+        pair.first = ip;
+        pair.second = prt;
     return pair;
-};
+}
