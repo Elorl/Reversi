ddd5cd2ac4644ca564ae602b94596b16a10fcb4f
avnermer <avnermer7@gmail.com>
2017-12-28 11:57:09 +0200

    .

--- src/server/server/Play.cpp
+++ src/server/server/Play.cpp
@@ -9,6 +9,22 @@
 
 int Play::execute(vector<void *> args) {
 
-    string name = *(string*)args[0];
-    map<string, Room>* rooms = (map*)args[1];
+    int destSocket = *(int*)args[0];
+    string name = *(string*)args[1];
+
+    int x = *(int*)args[2];
+    int y = *(int*)args[3];
+    map<string, Room*>* rooms = (map*)args[4];
+
+    //found room - if not found , it will contain the end of the map
+    Room* foundRoom = rooms->find(name)->second;
+    //if a game with such does not exist, or room is not running yet
+    if(rooms->find(name) == rooms->end() || !foundRoom->isRunning()){
+        return -1;
+    }
+
+    //write move to socket
+    foundRoom->writeNumToSocket(x, destSocket);
+    foundRoom->writeNumToSocket((int)",", destSocket);
+    foundRoom->writeNumToSocket(y, destSocket);
 }
