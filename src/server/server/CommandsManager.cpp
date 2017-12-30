a1656d7d0ef6bc00a2151f10e97325701002e090
avnermer <avnermer7@gmail.com>
2017-12-30 18:37:00 +0200

    .

--- src/server/server/CommandsManager.cpp
+++ src/server/server/CommandsManager.cpp
@@ -3,7 +3,22 @@
 //
 
 #include "CommandsManager.h"
+#include "Start.h"
+#include "Join.h"
+#include "Play.h"
+#include "Close.h"
 
+CommandsManager::CommandsManager() {
+    Command start = start();
+    Command join = Join();
+    Command play = Play();
+    Command close = Close();
+    commandsMap.insert(pair<string, Command&>("start", start));
+    commandsMap.insert(pair<string, Command&>("join", join));
+    commandsMap.insert(pair<string, Command&>("play", play));
+    commandsMap.insert(pair<string, Command&>("close", close));
+
+}
 int CommandsManager::executeCommand(string command, vector<string> args) {
     Command foundCommand = commandsMap.find(command)->second;
 
