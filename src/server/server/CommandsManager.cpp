6fe6f20fa619b25f07a8d8f0789ffb4630da9a3d
avnermer <avnermer7@gmail.com>
2017-12-30 18:51:57 +0200

    Merge branch 'master' of https://github.com/Elorl/Reversi
    # Conflicts:
    #	src/server/server/CommandsManager.cpp

--- src/server/server/CommandsManager.cpp
+++ src/server/server/CommandsManager.cpp
@@ -7,16 +7,19 @@
 #include "Join.h"
 #include "Play.h"
 #include "Close.h"
+#include "ListGame.h"
 
 CommandsManager::CommandsManager() {
-    Command start = start();
+    Command start = Start();
     Command join = Join();
     Command play = Play();
     Command close = Close();
+    Command listGame = ListGame();
     commandsMap.insert(pair<string, Command&>("start", start));
     commandsMap.insert(pair<string, Command&>("join", join));
     commandsMap.insert(pair<string, Command&>("play", play));
     commandsMap.insert(pair<string, Command&>("close", close));
+    commandsMap.insert(pair<string, Command&>("list_games", listGame));
 
 }
 int CommandsManager::executeCommand(string command, vector<string> args) {
